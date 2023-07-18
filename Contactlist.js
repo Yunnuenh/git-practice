82961a2e78524ae00fdf13fa49d800f0bbc49439
yunnuen.hernandez <yunnuenh807@gmail.com>
2023-07-17 19:58:03 -0600

    Subirrepositorio2

--- Contactlist.js
+++ Contactlist.js
@@ -1,20 +1,55 @@
-let Contactlist = ["Maria Lobo", "Nick Hill", "Fabiana Hernandez"];
-function agregarContacto(contacto) {
-  Contactlist.push(contacto);
-  console.log("Contacto añadido:", contacto);
-}
-function borrarContacto(contacto) {
-  let indice = Contactlist.indexOf(contacto);
-  if (indice !== -1) {
-    Contactlist.splice(indice, 1);
-    console.log("Contacto borrado:", contacto);
-  } else {
-    console.log("El contacto no existe en la lista.");
+let Contactlist = [
+    {
+      id: 1,
+      nombres: "Maria",
+      apellidos: "Lobo",
+      telefono: "50487322133",
+      ubicaciones: {
+        ciudad: "Villanueva, Cortes",
+        direccion: "3 calle 2 avenida"
+      }
+    },
+    {
+      id: 2,
+      nombres: "Nick",
+      apellidos: "Hill",
+      telefono: "50498016097",
+      ubicaciones: {
+        ciudad: "Saba,Colon",
+        direccion: "Calle Principal"
+      }
+    },
+    {
+      id: 3,
+      nombres: "Fabiana",
+      apellidos: "Hernandez",
+      telefono: "50583896720",
+      ubicaciones: {
+        ciudad: "Managua,Nicaragua",
+        direccion: "Calle Principal"
+      }
+    }
+  ];
+  
+  function agregarContacto(contacto) {
+    Contactlist.push(contacto);
+    console.log("Contacto añadido:", contacto);
   }
-}
-function imprimirContactos() {
-  console.log("Lista de contactos:");
-  for (let contacto of Contactlist) {
-    console.log(contacto);
+  
+  function borrarContacto(contacto) {
+    let indice = Contactlist.findIndex(c => c.id === contacto.id);
+    if (indice !== -1) {
+      let nombreContacto = Contactlist[indice].nombres + " " + Contactlist[indice].apellidos;
+      Contactlist.splice(indice, 1);
+      console.log("Contacto borrado:", nombreContacto);
+    } else {
+      console.log("El contacto no existe en la lista.");
+    }
   }
-}
+  
+  function imprimirContactos() {
+    console.log("Lista de contactos:");
+    for (let contacto of Contactlist) {
+      console.log(contacto.nombres + " " + contacto.apellidos);
+    }
+  }
